fa3d6e663013b3dd9a499de6391c6f3485a6e2cc
Guillaume Duran <guillaumeduran2@gmail.com>
2023-07-20 12:08:53 +0200

    logic ok

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,5 +1,63 @@
+import { useEffect, useState } from "react";
+
 const Quote = () => {
-	return <h1>Quote</h1>;
+	const [random, setRandom] = useState({});
+	const [author, setAuthor] = useState([]);
+	useEffect(() => {
+		getRandomQuote();
+	}, []);
+
+	const getRandomQuote = async () => {
+		try {
+			const response = await fetch(
+				"https://quote-garden.onrender.com/api/v3/quotes/random"
+			);
+			const data = await response.json();
+			setRandom(data.data[0]);
+			setAuthor({});
+		} catch (error) {
+			console.log(error);
+			return;
+		}
+	};
+	const getAuthorQuotes = async (author) => {
+		try {
+			const response = await fetch(
+				`https://quote-garden.onrender.com/api/v3/quotes?author=${author}`
+			);
+			const data = await response.json();
+			setAuthor(data.data);
+		} catch (error) {
+			console.log(error);
+			return;
+		}
+	};
+	return (
+		<>
+			<div>
+				<button onClick={() => getRandomQuote()}>random</button>
+			</div>
+			{random.quoteAuthor && !author.length && (
+				<div>
+					<blockquote>{random.quoteText}</blockquote>
+					<div onClick={() => getAuthorQuotes(random.quoteAuthor)}>
+						<h1>{random.quoteAuthor}</h1>
+						<small>{random.quoteGenre}</small>
+					</div>
+				</div>
+			)}
+			{author.length && (
+				<div>
+					<h1>{random.quoteAuthor}</h1>
+					{author.map((item, index) => (
+						<div key={index}>
+							<blockquote>{item.quoteText}</blockquote>
+						</div>
+					))}
+				</div>
+			)}
+		</>
+	);
 };
 
 export default Quote;
