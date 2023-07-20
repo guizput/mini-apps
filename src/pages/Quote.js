c1a747a9a97a11e83e32a17958bea98970026912
Guillaume Duran <guillaumeduran2@gmail.com>
2023-07-20 14:54:39 +0200

    ready for prod

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,18 +1,24 @@
 import { useEffect, useState } from "react";
+import Loading from "./quote/Loading";
+import RandomIcon from "./quote/RandomIcon";
+import ArrowIcon from "./quote/ArrowIcon";
 
 const Quote = () => {
 	const [random, setRandom] = useState({});
 	const [author, setAuthor] = useState([]);
+	const [loading, setLoading] = useState(true);
 	useEffect(() => {
 		getRandomQuote();
 	}, []);
 
 	const getRandomQuote = async () => {
+		setLoading(true);
 		try {
 			const response = await fetch(
 				"https://quote-garden.onrender.com/api/v3/quotes/random"
 			);
 			const data = await response.json();
+			setLoading(false);
 			setRandom(data.data[0]);
 			setAuthor({});
 		} catch (error) {
@@ -21,11 +27,13 @@ const Quote = () => {
 		}
 	};
 	const getAuthorQuotes = async (author) => {
+		setLoading(true);
 		try {
 			const response = await fetch(
 				`https://quote-garden.onrender.com/api/v3/quotes?author=${author}`
 			);
 			const data = await response.json();
+			setLoading(false);
 			setAuthor(data.data);
 		} catch (error) {
 			console.log(error);
@@ -33,30 +41,50 @@ const Quote = () => {
 		}
 	};
 	return (
-		<>
-			<div>
-				<button onClick={() => getRandomQuote()}>random</button>
-			</div>
-			{random.quoteAuthor && !author.length && (
-				<div>
-					<blockquote>{random.quoteText}</blockquote>
-					<div onClick={() => getAuthorQuotes(random.quoteAuthor)}>
-						<h1>{random.quoteAuthor}</h1>
-						<small>{random.quoteGenre}</small>
+		<div className="flex h-full items-center justify-center p-4">
+			{loading && <Loading />}
+			<button
+				className="absolute right-4 top-4 z-10 flex items-center justify-between rounded-md bg-gray-100 p-2 hover:bg-gray-200"
+				onClick={() => getRandomQuote()}
+			>
+				random <RandomIcon />
+			</button>
+			{!loading && random.quoteAuthor && !author.length && (
+				<div className="max-w-md">
+					<blockquote className="border-l-4 border-amber-300 pl-12 text-lg">
+						"{random.quoteText}"
+					</blockquote>
+					<div
+						className="group ml-12 mt-12 flex cursor-pointer items-center justify-between p-4 hover:bg-gray-900"
+						onClick={() => getAuthorQuotes(random.quoteAuthor)}
+					>
+						<div>
+							<h1 className="text-gray-700 group-hover:text-white">
+								{random.quoteAuthor}
+							</h1>
+							<small className="text-sm text-gray-500 group-hover:text-white">
+								{random.quoteGenre}
+							</small>
+						</div>
+						<ArrowIcon className="hidden group-hover:visible" />
 					</div>
 				</div>
 			)}
-			{author.length && (
-				<div>
-					<h1>{random.quoteAuthor}</h1>
+			{!loading && author.length && (
+				<div className="absolute left-1/2 top-0 w-full max-w-lg -translate-x-1/2 px-4 py-12">
+					<h1 className="mb-20 ml-12 text-xl font-medium">
+						{random.quoteAuthor}
+					</h1>
 					{author.map((item, index) => (
 						<div key={index}>
-							<blockquote>{item.quoteText}</blockquote>
+							<blockquote className="mb-20 border-l-4 border-amber-300 pl-12 text-lg">
+								"{item.quoteText}"
+							</blockquote>
 						</div>
 					))}
 				</div>
 			)}
-		</>
+		</div>
 	);
 };
 
